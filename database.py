8d68dccb1c5c6b12ccc4eef7732cce2ef081b663
nikhil95 <105496233+nikhil95@users.noreply.github.com>
2022-10-26 20:55:17 +0530

    Update database.py

--- database.py
+++ database.py
@@ -17,17 +17,5 @@ database = databases.Database(DATABASE_URL)
 
 metadata = sqlalchemy.MetaData()
 
-notes = sqlalchemy.Table(
-    "notes",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("text", sqlalchemy.String),
-    sqlalchemy.Column("completed", sqlalchemy.Boolean),
-)
-
-engine = sqlalchemy.create_engine(
-    #DATABASE_URL, connect_args={"check_same_thread": False}
-    DATABASE_URL, pool_size=3, max_overflow=0
-)
 Base = declarative_base()
 
